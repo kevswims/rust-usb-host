69f115d3b8ae4db5c9367ae4aa9663cdb78d2134
Kevin Lannen <kevin.lannen@gmail.com>
2021-08-13 15:28:21 -0600

    Use libusb to read device info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut context = libusb::Context::new().unwrap();
+
+    for mut device in context.devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+
+        if device_desc.vendor_id() == 0x16c0 {
+            println!("Found our device");
+            println!(
+                "Bus {:03} Device {:03} ID {:04x}:{:04x}",
+                device.bus_number(),
+                device.address(),
+                device_desc.vendor_id(),
+                device_desc.product_id()
+            );
+        }
+    }
 }
