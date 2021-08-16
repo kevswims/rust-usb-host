e309131f19decbe45bd86559dfc5c6e73beaefe8
Kevin Lannen <kevin.lannen@gmail.com>
2021-08-15 21:18:27 -0600

    In the middle of stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-fn main() {
-    let mut context = libusb::Context::new().unwrap();
+use std::time::Duration;
 
-    for mut device in context.devices().unwrap().iter() {
+// use libusb::request_type;
+// use libusb::{Direction, Recipient, RequestType};
+fn main() {
+    for device in rusb::devices().unwrap().iter() {
         let device_desc = device.device_descriptor().unwrap();
 
         if device_desc.vendor_id() == 0x16c0 {
@@ -13,6 +15,29 @@ fn main() {
                 device_desc.vendor_id(),
                 device_desc.product_id()
             );
+
+            let mut dev = device.open().unwrap();
+
+            dev.claim_interface(0).unwrap();
+            println!("Opened device");
+
+            let languages = dev.read_languages(Duration::from_secs(1)).unwrap();
+
+            println!("{:?}", languages);
+
+            // let request_type = request_type(Direction::Out, RequestType::Vendor, Recipient::Device);
+            // let data = [1, 2, 3];
+
+            //     device.write_control(request_type, 13, 0, 0, &data, Duration::from_secs(1))
+            //         .unwrap();
+            // let languages = device.read_languages(Duration::from_secs(1)).unwrap();
+
+            // println!("{:?}", languages);
+            // let us_english = libusb::language::from_lang_id(0x0409);
+
+            // let mfg_string = device.read_manufacturer_string()
+
+            // let class_code = device_desc.class_code();
         }
     }
 }
